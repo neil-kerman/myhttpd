c5939bf16078b894fbcbf35ed7d896da878ee469
ansar enver <ansar_enver@outlook.com>
2023-03-08 01:31:26 +0800

    add linked_buffer unit test

--- src/util/linked_buffer.hpp
+++ src/util/linked_buffer.hpp
@@ -4,11 +4,15 @@
 
 
 namespace myhttpd {
-#define BlockSize 4096
-    //template <typename BlockSize>
+
+    template <std::size_t BlockSize>
     class linked_buffer {
 
+#ifdef __MYHTTPD_UNIT_TEST__
+    public:
+#else
     private:
+#endif
         typedef std::array<char, BlockSize> BlockType;
     
     public:
@@ -17,46 +21,71 @@ namespace myhttpd {
             std::size_t size;
         };
 
+#ifdef __MYHTTPD_UNIT_TEST__
+    public:
+#else
     private:
+#endif
         std::size_t _size = 0;
 
-        std::size_t _capacity;
+        std::size_t _capacity = 0;
 
         std::size_t _offset = 0;
 
         std::list<BlockType> _blocks;
 
-    private:
-
     public:
-        linked_buffer(std::size_t blocks = 1)
-        : _blocks(std::list<BlockType>(blocks), _capacity(this->_blocks.size()* BlcokSize) {}
-
+        linked_buffer() {};
 
         linked_buffer(linked_buffer&& buf) 
         : _blocks(std::move(buf._blocks)), _size(buf._size), _capacity(buf._capacity) {}
 
-        linked_buffer(const linked_buffer& buf)
-        : _blocks(std::copy(buf._blocks)), _size(buf._size), _capacity(buf._capacity) {}
-
         void consume(std::size_t size) {
-
+            this->_size -= size;
+            this->_offset += size;
+            while(this->_offset >= BlockSize) {
+                this->_offset -= BlockSize;
+                this->_blocks.pop_front();
+            }
         }
 
         void commit(std::size_t size) {
-
+            this->_size += size;
+            this->_capacity -= size;
         }
 
         std::size_t size() {
-
+            return this->_size;
         }
 
-        std::list<block> get_available_block() {
-
+        block get_available_block() {
+            if (this->_capacity == 0) {
+                this->_blocks.push_back(BlockType());
+                this->_capacity += BlockSize;
+                return block{ this->_blocks.back().data(), BlockSize };
+            } else {
+                return block{ this->_blocks.back().data() + ((this->_offset + this->_size) % BlockSize), this->_capacity};
+            }
         }
 
-        std::list<block> get_data_block() {
-
+        std::list<block> get_data(std::size_t size) {
+            std::list<block> data_blocks;
+            if (size <= this->_size) {
+                auto first_block_size = (size > (BlockSize - this->_offset)) ? BlockSize - this->_offset : size;
+                auto full_blocks = (size - this->_offset) / BlockSize;
+                auto last_block_size = size - (full_blocks * BlockSize) - first_block_size;
+                auto it = this->_blocks.begin();
+                data_blocks.push_back(block{ it->data() + this->_offset, first_block_size });
+                it++;
+                for (std::size_t i = 0; i < full_blocks; i++) {
+                    data_blocks.push_back(block{ it->data(), BlockSize });
+                    it++;
+                }
+                if (last_block_size != 0) {
+                    data_blocks.push_back(block{ it->data(), last_block_size });
+                }
+            }
+            return data_blocks;
         }
     };
 }
