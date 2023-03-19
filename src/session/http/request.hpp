e3f1b3615022eca2627d8362b8a1c559c488c2dc
ansar enver <ansar_enver@outlook.com>
2023-03-19 16:16:05 +0800

    Update request.hpp and response.hpp

--- src/session/http/request.hpp
+++ src/session/http/request.hpp
@@ -3,23 +3,97 @@
 
 #include "message.hpp"
 
+#undef DELETE
+
 namespace myhttpd::http {
 
     class request: public message {
 
+    public:
+        enum method {
+            OPTIONS,
+            GET,
+            HEAD,
+            POST,
+            PUT,
+            DELETE,
+            TRACE,
+            CONNECT,
+            UNKNOWN,
+        };
+
     private:
-        std::string _method;
+        method _method;
 
         std::string _url;
 
         std::string _version;
 
     private:
+        static method _method_parse(std::string met_str) {
+
+            if (met_str == "OPTIONS") {
+                return method::OPTIONS;
+            } else if (met_str == "GET") {
+                return method::GET;
+            } else if (met_str == "HEAD") {
+                return method::HEAD;
+            } else if (met_str == "POST") {
+                return method::POST;
+            } else if (met_str == "PUT") {
+                return method::PUT;
+            } else if (met_str == "DELETE") {
+                return method::DELETE;
+            } else if (met_str == "TRACE") {
+                return method::TRACE;
+            } else if (met_str == "CONNECT") {
+                return method::CONNECT;
+            } else {
+                return method::UNKNOWN;
+            }
+        }
+
+        static const std::string& _method_to_string(method met) {
+
+            static const std::string method_options_str = "OPTIONS";
+            static const std::string method_get_str = "GET";
+            static const std::string method_head_str = "HEAD";
+            static const std::string method_post_str = "POST";
+            static const std::string method_put_str = "PUT";
+            static const std::string method_delete_str = "DELETE";
+            static const std::string method_trace_str = "TRACE";
+            static const std::string method_connect_str = "CONNECT";
+            static const std::string method_unknown_str = "CONNECT";
+
+            switch (met) {
+            case method::OPTIONS:
+                return method_options_str;
+            case method::GET:
+                return method_get_str;
+            case method::HEAD:
+                return method_head_str;
+            case method::POST:
+                return method_post_str;
+            case method::PUT:
+                return method_put_str;
+            case method::DELETE:
+                return method_delete_str;
+            case method::TRACE:
+                return method_trace_str;
+            case method::CONNECT:
+                return method_connect_str;
+            default:
+                return method_unknown_str;
+            }
+        }
+
+
         void _extract_title() {
             auto title = message::get_title();
             std::size_t offset = 0;
             auto size = title.find(' ', 0);
-            this->_method = title.substr(0, size);
+            auto method_str = title.substr(0, size);
+            this->_method = this->_method_parse(method_str);
             offset = size + 1;
             size = title.find(' ', offset) - offset;
             this->_url = title.substr(offset, size);
@@ -37,12 +111,12 @@ namespace myhttpd::http {
             this->_version = version;
         }
 
-        inline std::string get_method() {
+        inline method get_method() {
             return this->_method;
         }
 
-        inline void set_method(std::string method) {
-            this->_method = method;
+        inline void set_method(method met) {
+            this->_method = met;
         }
 
         inline std::string get_url() {
@@ -55,6 +129,8 @@ namespace myhttpd::http {
 
     public:
         virtual std::string get_title() {
+            std::string title;
+            std::size_t size = 0;
             return (this->_method + " " + this->_url + " " + this->_version);
         }
 
--- src/session/http/response.hpp
+++ src/session/http/response.hpp
@@ -13,6 +13,97 @@ namespace myhttpd::http {
         unsigned _status;
 
     private:
+        static const std::string& _get_status_meaning(unsigned status) {
+
+            static const std::string status_100_meaning = "continue";
+            static const std::string status_101_meaning = "switching protocols";
+            static const std::string status_200_meaning = "ok";
+            static const std::string status_201_meaning = "created";
+            static const std::string status_202_meaning = "accepted";
+            static const std::string status_203_meaning = "non-authoritative information";
+            static const std::string status_204_meaning = "no content";
+            static const std::string status_205_meaning = "reset content";
+            static const std::string status_206_meaning = "partial content";
+            static const std::string status_300_meaning = "multiple choices";
+            static const std::string status_301_meaning = "moved permanently";
+            static const std::string status_302_meaning = "found";
+            static const std::string status_303_meaning = "see other";
+            static const std::string status_304_meaning = "not modified";
+            static const std::string status_305_meaning = "use proxy";
+            static const std::string status_306_meaning = "*";
+            static const std::string status_307_meaning = "temporary redirect";
+            static const std::string status_400_meaning = "bad request";
+            static const std::string status_401_meaning = "unauthorized";
+            static const std::string status_402_meaning = "payment required";
+            static const std::string status_403_meaning = "forbidden";
+            static const std::string status_404_meaning = "not found";
+            static const std::string status_405_meaning = "method not allowed";
+            static const std::string status_406_meaning = "not acceptable";
+            static const std::string status_407_meaning = "proxy authentication required";
+            static const std::string status_408_meaning = "request timeout";
+            static const std::string status_409_meaning = "conflict";
+            static const std::string status_410_meaning = "gone";
+            static const std::string status_411_meaning = "length required";
+            static const std::string status_412_meaning = "precondition failed";
+            static const std::string status_413_meaning = "request entity too large";
+            static const std::string status_414_meaning = "request-uri too long";
+            static const std::string status_415_meaning = "unsupported media type";
+            static const std::string status_416_meaning = "requested range not satisfiable";
+            static const std::string status_417_meaning = "expectation failed";
+            static const std::string status_500_meaning = "internal server error";
+            static const std::string status_501_meaning = "not implemented";
+            static const std::string status_502_meaning = "bad gateway";
+            static const std::string status_503_meaning = "service unavailable";
+            static const std::string status_504_meaning = "gateway timeout";
+            static const std::string status_505_meaning = "http version not supported";
+            static const std::string status_undefined_meaning = "*";
+
+            switch (status) {
+            case 100: return status_100_meaning;
+            case 101: return status_101_meaning;
+            case 200: return status_200_meaning;
+            case 201: return status_201_meaning;
+            case 202: return status_202_meaning;
+            case 203: return status_203_meaning;
+            case 204: return status_204_meaning;
+            case 205: return status_205_meaning;
+            case 206: return status_206_meaning;
+            case 300: return status_300_meaning;
+            case 301: return status_301_meaning;
+            case 302: return status_302_meaning;
+            case 303: return status_303_meaning;
+            case 304: return status_304_meaning;
+            case 305: return status_305_meaning;
+            case 306: return status_306_meaning;
+            case 307: return status_307_meaning;
+            case 400: return status_400_meaning;
+            case 401: return status_401_meaning;
+            case 402: return status_402_meaning;
+            case 403: return status_403_meaning;
+            case 404: return status_404_meaning;
+            case 405: return status_405_meaning;
+            case 406: return status_406_meaning;
+            case 407: return status_407_meaning;
+            case 408: return status_408_meaning;
+            case 409: return status_409_meaning;
+            case 410: return status_410_meaning;
+            case 411: return status_411_meaning;
+            case 412: return status_412_meaning;
+            case 413: return status_413_meaning;
+            case 414: return status_414_meaning;
+            case 415: return status_415_meaning;
+            case 416: return status_416_meaning;
+            case 417: return status_417_meaning;
+            case 500: return status_500_meaning;
+            case 501: return status_501_meaning;
+            case 502: return status_502_meaning;
+            case 503: return status_503_meaning;
+            case 504: return status_504_meaning;
+            case 505: return status_505_meaning;
+            default: return status_undefined_meaning;
+            }
+        }
+
         void _extract_title() {
             auto title = message::get_title();
             std::size_t offset = 0;
@@ -25,7 +116,7 @@ namespace myhttpd::http {
         }
 
     public:
-        inline std::string get_version() {
+        inline std::string& get_version() {
             return this->_version;
         }
 
@@ -43,7 +134,12 @@ namespace myhttpd::http {
 
     public:
         virtual std::string get_title() {
-            return (this->_version + " " + std::to_string(this->_status) + "TODO");
+            auto& status_meaning = this->_get_status_meaning(this->_status);
+            auto& version = this->get_version();
+            std::string title;
+            title.reserve(version.size() + 5 + status_meaning.size());
+            title.append(version).append(" ").append(std::to_string(this->_status)).append(status_meaning);
+            return title;
         }
 
         virtual void set_title(std::string title) {
